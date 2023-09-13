f9adc4452b0108f42921c2167af763fd805fa3dd
Ferganskiy <odilovmadiyor029@gmail.com>
2023-09-13 17:02:38 +0500

    VehicleService added

--- Recore.Service/Services/VehicleService.cs
+++ Recore.Service/Services/VehicleService.cs
@@ -1,32 +1,74 @@
-﻿using Recore.Service.DTOs.Vehicles;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Recore.Data.IRepositories;
+using Recore.Domain.Entities.Suppliers;
+using Recore.Domain.Entities.Users;
+using Recore.Service.DTOs.Users;
+using Recore.Service.DTOs.Vehicles;
+using Recore.Service.Exceptions;
+using Recore.Service.Helpers;
 using Recore.Service.Interfaces;
 
 namespace Recore.Service.Services;
 
 public class VehicleService : IVehicleService
 {
-    public Task<VehicleResultDto> AddAsync(VehicleCreationDto dto)
+    private readonly IMapper mapper;
+    private readonly IRepository<Vehicle> repository;
+    public VehicleService(IRepository<Vehicle> repository, IMapper mapper)
     {
-        throw new NotImplementedException();
+        this.mapper = mapper;
+        this.repository = repository;
+    }
+    public async Task<VehicleResultDto> AddAsync(VehicleCreationDto dto)
+    {
+        var mappedVehicle = this.mapper.Map<Vehicle>(dto);
+        await this.repository.CreateAsync(mappedVehicle);
+        await this.repository.SaveAsync();
+
+        var result = this.mapper.Map<VehicleResultDto>(mappedVehicle);
+        return result;
     }
 
-    public Task<VehicleResultDto> ModifyAsync(VehicleUpdateDto dto)
+    public async Task<VehicleResultDto> ModifyAsync(VehicleUpdateDto dto)
     {
-        throw new NotImplementedException();
+        Vehicle vehicle = await this.repository.SelectAsync(u => u.Id.Equals(dto.Id));
+        if (vehicle is null)
+            throw new NotFoundException($"This vehicle is not found with ID = {dto.Id}");
+
+        this.mapper.Map(dto, vehicle);
+        this.repository.Update(vehicle);
+        await this.repository.SaveAsync();
+
+        var result = this.mapper.Map<VehicleResultDto>(vehicle);
+        return result;
     }
 
-    public Task<bool> RemoveAsync(long id)
+    public async Task<bool> RemoveAsync(long id)
     {
-        throw new NotImplementedException();
+        Vehicle vehicle = await this.repository.SelectAsync(u => u.Id.Equals(id));
+        if (vehicle is null)
+            throw new NotFoundException($"This vehicle is not found with ID = {id}");
+
+        this.repository.Delete(vehicle);
+        await this.repository.SaveAsync();
+        return true;
     }
 
-    public Task<IEnumerable<VehicleResultDto>> RetrieveAllAsync()
+    public async Task<IEnumerable<VehicleResultDto>> RetrieveAllAsync()
     {
-        throw new NotImplementedException();
+        var vehicle = await this.repository.SelectAll().ToListAsync();
+        var result = this.mapper.Map<IEnumerable<VehicleResultDto>>(vehicle);
+        return result;
     }
 
-    public Task<VehicleResultDto> RetrieveByIdAsync(long id)
+    public async Task<VehicleResultDto> RetrieveByIdAsync(long id)
     {
-        throw new NotImplementedException();
+        Vehicle vehicle = await this.repository.SelectAsync(u => u.Id.Equals(id));
+        if (vehicle is null)
+            throw new NotFoundException($"This vehicle is not found with ID = {id}");
+
+        var result = this.mapper.Map<VehicleResultDto>(vehicle);
+        return result;
     }
 }
