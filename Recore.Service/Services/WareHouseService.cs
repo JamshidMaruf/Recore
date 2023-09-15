fa92af3a9a9987fb7d321b525554ba867ff5aa42
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-09-15 19:10:57 +0500

    Service add

--- Recore.Service/Services/WareHouseService.cs
+++ Recore.Service/Services/WareHouseService.cs
@@ -1,12 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using Recore.Data.IRepositories;
+using Recore.Service.Exceptions;
+using Recore.Service.Extensions;
+using Recore.Service.Interfaces;
+using Recore.Domain.Configurations;
+using Microsoft.EntityFrameworkCore;
+using Recore.Service.DTOs.WareHouses;
+using Recore.Domain.Entities.WareHouses;
 
-namespace Recore.Service.Services
+namespace Recore.Service.Services;
+
+public class WareHouseService : IWareHouseService
 {
-	internal class WareHouseService
+	private readonly IMapper mapper;
+	private readonly IRepository<WareHouse> repository;
+	public WareHouseService(IRepository<WareHouse> repository, IMapper mapper)
 	{
+		this.mapper = mapper;
+		this.repository = repository;
+	}
+
+	public async ValueTask<WareHouseResultDto> AddAsync(WareHouseCreationDto dto)
+	{
+		WareHouse existWareHouse = await this.repository.SelectAsync(u => u.Name.Equals(dto.Name));
+		if (existWareHouse is not null)
+			throw new AlreadyExistException($"This WareHouse is already exists with phone = {dto.Name}");
+
+		var mappedWareHouse = this.mapper.Map<WareHouse>(dto);
+		await this.repository.CreateAsync(mappedWareHouse);
+		await this.repository.SaveAsync();
+
+		var result = this.mapper.Map<WareHouseResultDto>(mappedWareHouse);
+		return result;
+	}
+
+	public async ValueTask<WareHouseResultDto> ModifyAsync(WareHouseUpdateDto dto)
+	{
+		WareHouse existWareHouse = await this.repository.SelectAsync(u => u.Id.Equals(dto.Id))
+			?? throw new NotFoundException($"This WareHouse is not found with ID = {dto.Id}");
+
+		this.mapper.Map(dto, existWareHouse);
+		this.repository.Update(existWareHouse);
+		await this.repository.SaveAsync();
+
+		var result = this.mapper.Map<WareHouseResultDto>(existWareHouse);
+		return result;
+	}
+
+	public async ValueTask<bool> RemoveAsync(long id)
+	{
+		WareHouse existWareHouse = await this.repository.SelectAsync(u => u.Id.Equals(id))
+			?? throw new NotFoundException($"This WareHouse is not found with ID = {id}");
+
+		this.repository.Delete(existWareHouse);
+		await this.repository.SaveAsync();
+		return true;
+	}
+
+	public async ValueTask<WareHouseResultDto> RetrieveByIdAsync(long id)
+	{
+		WareHouse existWareHouse = await this.repository.SelectAsync(u => u.Id.Equals(id))
+			?? throw new NotFoundException($"This WareHouse is not found with ID = {id}");
+
+		var result = this.mapper.Map<WareHouseResultDto>(existWareHouse);
+		return result;
+	}
+
+	public async ValueTask<IEnumerable<WareHouseResultDto>> RetrieveAllAsync(PaginationParams @params, string search = null)
+	{
+		var WareHouses = await this.repository.SelectAll()
+			.ToPaginate(@params)
+			.ToListAsync();
+
+		var result = WareHouses.Where(WareHouse => WareHouse.Name.Contains(search, StringComparison.OrdinalIgnoreCase));
+		var mappedWareHouses = this.mapper.Map<List<WareHouseResultDto>>(result);
+		return mappedWareHouses;
+
+		//var result = this.mapper.Map<IEnumerable<WareHouseResultDto>>(WareHouses);
+		//return result;
+	}
+
+	public async ValueTask<IEnumerable<WareHouseResultDto>> RetrieveAllAsync()
+	{
+		var WareHouses = await this.repository.SelectAll()
+			.ToListAsync();
+		var result = this.mapper.Map<IEnumerable<WareHouseResultDto>>(WareHouses);
+		return result;
 	}
 }
