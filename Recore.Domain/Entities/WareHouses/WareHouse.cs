5c4fd8f771242ea32d1c71ea1a0ad1eb6bfcd92d
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-09-15 17:22:24 +0500

    Endi qo'shilsa keragovv

--- Recore.Domain/Entities/WareHouses/WareHouse.cs
+++ Recore.Domain/Entities/WareHouses/WareHouse.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Recore.Domain.Entities.WareHouses
+﻿namespace Recore.Domain.Entities.WareHouses
 {
-	internal class WareHouse
+	public class WareHouse
 	{
+		public string FirstName { get; set; }
 	}
 }
