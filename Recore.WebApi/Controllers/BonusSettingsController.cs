22f010a7aee345b410926472e9b329c7200b0d8e
Nurullo <ghostone234qwer@gmail.com>
2023-09-15 13:36:50 +0500

    Add bonus settings controller

--- Recore.WebApi/Controllers/BonusSettingsController.cs
+++ Recore.WebApi/Controllers/BonusSettingsController.cs
@@ -1,6 +1,65 @@
-﻿namespace Recore.WebApi.Controllers
+﻿using Recore.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Recore.Service.Interfaces;
+using Recore.Domain.Configurations;
+using Recore.Service.DTOs.BonusSetting;
+
+namespace Recore.WebApi.Controllers;
+
+public class BonusSettingsController : BaseController
 {
-    public class BonusSettingsController
+    private readonly IBonusSettingService bonusSettingService;
+    public BonusSettingsController(IBonusSettingService bonusSettingService)
     {
+        this.bonusSettingService = bonusSettingService;
     }
+
+    [HttpPost("create")]
+    public async ValueTask<IActionResult> PostAsync(BonusSettingCreationDto dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this.bonusSettingService.AddAsync(dto)
+        });
+
+
+    [HttpPut("update")]
+    public async ValueTask<IActionResult> PutAsync(BonusSettingUpdateDto dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this.bonusSettingService.ModifyAsync(dto)
+        });
+
+
+    [HttpDelete("delete/{id:long}")]
+    public async ValueTask<IActionResult> DeleteAsync(long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this.bonusSettingService.RemoveAsync(id)
+        });
+
+
+    [HttpGet("get/{id:long}")]
+    public async ValueTask<IActionResult> GetByIdAsync(long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this.bonusSettingService.RetrieveByIdAsync(id)
+        });
+
+
+    [HttpGet("get-all")]
+    public async ValueTask<IActionResult> GetAllAsync([FromQuery] PaginationParams @params)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this.bonusSettingService.RetrieveAllAsync(@params)
+        });
 }
