05b9ee0c85a2a6b1bb15ae20fab10fcd7eeeb87f
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-09-18 17:21:58 +0500

    Errr connections

--- Recore.WebApi/Controllers/WareHouseController.cs
+++ Recore.WebApi/Controllers/WareHouseController.cs
@@ -1,6 +1,78 @@
-﻿namespace Recore.WebApi.Controllers
+﻿using Recore.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Recore.Service.Interfaces;
+using Recore.Domain.Configurations;
+using Recore.Service.DTOs.WareHouses;
+
+
+
+namespace Recore.WebApi.Controllers
 {
-	public class WareHouseController
+	public class WareHouseController : Controller
 	{
+		private readonly IWareHouseService wareHouseService;
+		public WareHouseController(IWareHouseService wareHouseService)
+		{
+			this.wareHouseService = wareHouseService;
+		}
+
+		/// <summary>
+		/// Create new WareHouse
+		/// </summary>
+		/// <param name="dto"></param>
+		/// <returns></returns>
+		[HttpPost]
+		public async ValueTask<IActionResult> PostAsync([FromBody] WareHouseCreationDto dto)
+			=> Ok(new Response
+			{
+				StatusCode = 200,
+				Message = "Success",
+				Data = await this.wareHouseService.AddAsync(dto)
+			});
+
+		/// <summary>
+		/// Delete by id
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		[HttpDelete("id")]
+		public async ValueTask<IActionResult> DeleteAsync(long id)
+			 => Ok(new Response
+			 {
+				 StatusCode = 200,
+				 Message = "Success",
+				 Data = await this.wareHouseService.RemoveAsync(id)
+			 });
+
+		/// <summary>
+		/// Get by id
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		[HttpGet("id")]
+		public async ValueTask<IActionResult> GetByIdAsync(long id)
+			 => Ok(new Response
+			 {
+				 StatusCode = 200,
+				 Message = "Success",
+				 Data = await this.wareHouseService.RetrieveByIdAsync(id)
+			 });
+
+
+		/// <summary>
+		/// Get all WareHouses
+		/// </summary>
+		/// <param name="params"></param>
+		/// <returns></returns>
+		[HttpGet("get-list")]
+		public async ValueTask<IActionResult> GetAllAsync([FromQuery] PaginationParams @params)
+			=> Ok(new Response
+			{
+				StatusCode = 200,
+				Message = "Success",
+				Data = await this.wareHouseService.RetrieveAllAsync(@params)
+
+			});
 	}
 }
+
