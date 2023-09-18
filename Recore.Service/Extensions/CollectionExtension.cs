0a55d8a6323be404f4edff0c0b10baa91fce43cb
Muqimjon <muqimjon.mr@gmail.com>
2023-09-18 17:47:38 +0500

    Added X-Pagination and change to HttpContextHelper class to support this

--- Recore.Service/Extensions/CollectionExtension.cs
+++ Recore.Service/Extensions/CollectionExtension.cs
@@ -1,5 +1,9 @@
 ﻿using MailKit.Search;
+using Newtonsoft.Json;
+using Recore.Domain.Commons;
 using Recore.Domain.Configurations;
+using Recore.Service.Exceptions;
+using Recore.Service.Helpers;
 
 namespace Recore.Service.Extensions;
 
@@ -10,4 +14,33 @@ public static class CollectionExtension
 		var source = values.Skip((@params.PageIndex - 1) * @params.PageSize).Take(@params.PageSize);
 		return source;
 	}
+
+    public static IQueryable<TEntity> ToPagedList<TEntity>(this IQueryable<TEntity> entities, PaginationParams @params)
+        where TEntity : Auditable
+    {
+        if (@params.PageSize == 0 && @params.PageIndex == 0)
+        {
+            @params = new PaginationParams()
+            {
+                PageSize = 10,
+                PageIndex = 1
+            };
+        }
+        var metaData = new PaginationMetaData(entities.Count(), @params);
+
+        var json = JsonConvert.SerializeObject(metaData);
+
+        if (HttpContextHelper.ResponseHeaders != null)
+        {
+            if (HttpContextHelper.ResponseHeaders.ContainsKey("X-Pagination"))
+                HttpContextHelper.ResponseHeaders.Remove("X-Pagination");
+
+            HttpContextHelper.ResponseHeaders.Add("X-Pagination", json);
+        }
+
+        return @params.PageIndex > 0 && @params.PageSize > 0 ?
+            entities.OrderBy(e => e.Id)
+                .Skip((@params.PageIndex - 1) * @params.PageSize).Take(@params.PageSize) :
+                    throw new CustomException(400, "Please, enter valid numbers");
+    }
 }
